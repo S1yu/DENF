a582b26798a508d639c81b0012b340907454b7f8
杨思宇 <39834256+S1yu@users.noreply.github.com>
2023-09-03 17:58:20 +0800

    Update dnef.py

--- dnef.py
+++ dnef.py
@@ -125,9 +125,119 @@ def deepboost(xtrain1,xtrain2,xtrain3,ytrain1,ytrain2,ytrain3,has_weight=True,de
             ytrain3 = np.r_[ytrain3, change_label1]
             ytrain3 = np.r_[ytrain3, change_label2]
             ytrain3 =np.delete(ytrain3,dellist3,axis=0)
+auc_s=[]
+pre_s=[]
+f1_s=[]
+scor_s=[]
+recall_s=[]
+roc_s=[]
+log_s=[]
+def getsorce(adaboost,xtest=None,ytest=None):
+    '''
 
 
+    :return:  sc,ac,f1,prc,recall,log
+    '''
+    from sklearn.metrics import roc_auc_score,f1_score
+    from sklearn.preprocessing import  OneHotEncoder
+    #sc=adaboost.score(xtest, ytest)
+    ypre=adaboost.predict(xtest)
+    sc=accuracy_score(ytest,ypre)
+    ont =OneHotEncoder()
+    if ont.fit_transform(ytest.reshape(-1,1)).shape[1]>2:
+        ytest=ont.fit_transform(ytest.reshape(-1,1)).reshape(-1,1).toarray()
+        ypre=ont.transform(ypre.reshape(-1,1)).reshape(-1,1).toarray()
+
+    ac=roc_auc_score(ytest,ypre,multi_class='ovr')
+    f1=f1_score(ytest,ypre)#f1_score(ytest,ypre,average='micro')
+    prc = precision_score(ytest,ypre)
+    recall = recall_score(ytest,ypre)
+    logs=log_loss(ytest,ypre)
+    print("log",logs)
+    print('acc',sc)
+    return sc,ac,f1,prc,recall,logs
+
+def estimator_acc(estimators_,X,y,):
+    score=accuracy_score(y, estimators_.predict(X))
+    #print("{:.3f}".format(score))
+    return score
+def final_ensembe(boost1,boost2=None,boost3=None,e=0.7):
+    new_estimators=[]
+    new_estimator_weights=[]
+    #log.info("xtest shape",xtest.shape)
+    for index,estimator in  enumerate(boost1.estimators_):
+        acc= estimator_acc(estimator,xtest,ytest)
+        if (acc)>=e:
+            new_estimators.append(estimator)
+        new_estimator_weights.append(boost1.estimator_weights_.tolist()[index])
+    if boost2:
+        for index,estimator in  enumerate(boost2.estimators_):
+            acc= estimator_acc(estimator,xtest,ytest)
+            if (acc)>=e:
+                new_estimators.append(estimator)
+            new_estimator_weights.append(boost2.estimator_weights_.tolist()[index])
+    if boost3:
+        for index,estimator in  enumerate(boost3.estimators_):
+            acc= estimator_acc(estimator,xtest,ytest)
+            if (acc)>=e:
+                new_estimators.append(estimator)
+            new_estimator_weights.append(boost3.estimator_weights_.tolist()[index])
+
+
+    boost1.estimators_=new_estimators
+    boost1.estimator_weights_=np.asarray(new_estimator_weights)
+
+    return boost1
 
 
     return  boost1 ,boost2 ,boost3
 # no noml : 8537 8527
+from sklearn.model_selection import *
+kf=KFold(n_splits=Fold,shuffle=True,random_state=42)
+dct6=DecisionTreeClassifier(max_depth=5)
+
+def kf_predict(X,Y):
+    for train_index ,test_index in kf.split(X,Y):
+        xtest=  X[test_index]
+        ytest=  Y[test_index]
+        ALL_xtrain =X[train_index]
+        ALL_ytrain= Y[train_index]
+        # xtrain1 = x_train[:int(x_train.shape[0]/2)]
+        # xtrain2 = x_train[-int(x_train.shape[0]/2):]
+        # ytrain1 = y_train[:int(y_train.shape[0]/2)]
+        # ytrain2 = y_train[-int(y_train.shape[0]/2):]
+        #tem1=np.c_[x_train,y_train]
+        # 每个抽取0.6的数据
+        train1_index,_=train_test_split(train_index,train_size=0.33,random_state=42)
+        train2_index,_=train_test_split(train_index,train_size=0.33,random_state=43)
+        train3_index,_=train_test_split(train_index,train_size=0.33,random_state=44)
+    
+        xtrain1=X[train1_index]
+        xtrain2=X[train2_index]
+        xtrain3=X[train3_index]
+        ytrain1=Y[train1_index]
+        ytrain2=Y[train2_index]
+        ytrain3=Y[train3_index]
+    
+        
+        boost1 ,boost2 ,boost3=deepboost(xtrain1,xtrain2,xtrain3,ytrain1,ytrain2,ytrain3,has_weight=True,
+          delsample=False,layers=layer,base_estimator=dct5)
+    
+    
+         #adaboost1 ,adaboost2 =myadaboost(xtrain1,xtrain2,ytrain1,ytrain2,has_weight=False)
+        from sklearn.metrics import roc_auc_score
+       
+    
+        ensemboost=aensembe(boost1,boost2,boost3,e=0,xtrain=ALL_xtrain,ytrain=ALL_ytrain)
+        ensemble_score1.append(getsorce(ensemboost,xtest,ytest)[0])
+        ensemble_auc0.append(getsorce(ensemboost,xtest,ytest)[1])
+        ensemble_f10.append(getsorce(ensemboost,xtest,ytest)[2])
+        ensemble_ALL_pre.append(getsorce(ensemboost,xtest,ytest)[3])
+        ensemble_ALL_recall.append(getsorce(ensemboost,xtest,ytest)[4])
+        ensemble_ALL_log.append(getsorce(ensemboost,xtest,ytest)[5])
+        hold=0.4
+        ensemboost= final_ensembe(boost1,boost2,boost3,e=hold,xtrain=ALL_xtrain,ytrain=ALL_ytrain)
+      
+        ensemble_score2.append(getsorce(ensemboost,xtest,ytest)[0])
+        ensemble_auc1.append(getsorce(ensemboost,xtest,ytest)[1])
+        ensemble_f11.append(getsorce(ensemboost,xtest,ytest)[1])
